0682f6042b5a294c44ff5d62ea68edd8bbfa923d
karl-andres <135479605+karl-andres@users.noreply.github.com>
2025-12-20 19:52:08 -0500

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Load dataset to pandas Dataframe
+df = pd.read_csv("hf://datasets/ailsntua/Chordonomicon/chordonomicon_v2.csv", low_memory=False)
+
+
+# Drop rows that do not contain spotify song id and chords
+mask = df["chords"].notna() & df["spotify_song_id"].notna()
+
+clean_df = df[mask].copy()
+print(clean_df)
