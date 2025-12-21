be874b622044aa5e391b56db65c20ae4b4f53b5a
karl-andres <135479605+karl-andres@users.noreply.github.com>
2025-12-21 00:47:44 -0500

    setup spotify web api

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ df = pd.read_csv("hf://datasets/ailsntua/Chordonomicon/chordonomicon_v2.csv", lo
 
 
 # Drop rows that do not contain spotify song id and chords
-mask = df["chords"].notna() & df["spotify_song_id"].notna()
+mask = df["chords"].notna() & df["spotify_song_id"].notna() & df["genres"].notna()
 
 clean_df = df[mask].copy()
-print(clean_df)
+
+print(clean_df.loc[0, "chords"])
